1540dd8452481348bfbcc165cc5bcf1472646c16
Cam MacDonald <camm@rocktechcomputer.com>
2022-06-02 14:49:32 -0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, StyleSheet } from 'react-native'
+
+import HomeScreen from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <HomeScreen />
+    </NavigationContainer>
   );
 }
 
